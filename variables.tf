4efa9549c4fa95a6c054cf05872589c30e56a098
Cees van de Griend <cees.van.de.griend@xantara-it.nl>
2023-02-22 11:27:52 +0100

    fix: RHEL 8.7

--- variables.tf
+++ variables.tf
@@ -25,3 +25,34 @@ variable "tags" {
     "createdby"   = "terraform"
   }
 }
+
+variable "linux_vm_size" {
+  type        = string
+  description = "Virtual machine size"
+  default     = "Standard_B2s"
+}
+
+variable "linux_vm_storage_account_type" {
+  type        = string
+  description = "Virtual machine storage account type"
+  default     = "Standard_LRS"
+}
+
+variable "linux_vm_image_publisher" {
+  type        = string
+  description = "Virtual machine source image publisher"
+  default     = "RedHat"
+}
+
+variable "linux_vm_image_offer" {
+  type        = string
+  description = "Virtual machine source image offer"
+  default     = "RHEL"
+}
+
+variable "linux_vm_image_sku" {
+  type        = string
+  description = "SKU for RHEL 8.7 Gen2"
+  default     = "87-gen2"
+}
+
--- vm.tf
+++ vm.tf
@@ -15,37 +15,48 @@ resource "tls_private_key" "private_ssh_key" {
   rsa_bits  = 4096
 }
 
+#
 # Create virtual machine
+# See: https://gmusumeci.medium.com/how-to-deploy-a-red-hat-enterprise-linux-rhel-vm-in-azure-using-terraform-90f3d413c783
+#
 resource "azurerm_linux_virtual_machine" "vm" {
-  count                 = 1
-  name                  = "${var.name_prefix}-vm"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  tags                  = var.tags
-  network_interface_ids = [azurerm_network_interface.nic.id]
-  size                  = "Standard_DS1_v2"
+  count                           = 1
+  name                            = "${var.name_prefix}-vm"
+  location                        = azurerm_resource_group.rg.location
+  resource_group_name             = azurerm_resource_group.rg.name
+  tags                            = var.tags
+  network_interface_ids           = [azurerm_network_interface.nic.id]
+  size                            = var.linux_vm_size
 
   os_disk {
-    name                 = "${var.name_prefix}-disk"
-    caching              = "ReadWrite"
-    storage_account_type = "Premium_LRS"
+    name                          = "${var.name_prefix}-disk"
+    caching                       = "ReadWrite"
+    storage_account_type          = var.linux_vm_storage_account_type
   }
 
   source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
+    publisher                     = var.linux_vm_image_publisher
+    offer                         = var.linux_vm_image_offer
+    sku                           = var.linux_vm_image_sku
+    version                       = "latest"
   }
 
-  computer_name                   = "${var.name_prefix}"
+  computer_name                   = "${var.name_vm}"
   admin_username                  = "${var.name_user}"
-  disable_password_authentication = true
+  admin_password                  = random_password.password.result
+  disable_password_authentication = false
 
   admin_ssh_key {
-    username   = "${var.name_user}"
-    public_key = tls_private_key.private_ssh_key.public_key_openssh
+    username                      = "${var.name_user}"
+    public_key                    = tls_private_key.private_ssh_key.public_key_openssh
   }
+  
+  custom_data                     = base64encode(data.template_file.linux-vm-cloud-init.rendered)
+}
+
+# Template for bootstrapping
+data "template_file" "linux-vm-cloud-init" {
+  template = file("azure-rhel.sh")
 }
 
 # resource "azurerm_image" "img" {
